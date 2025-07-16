2be5c48d6b7ad2c88ec0f0529e2a633f6617fe12
Rowen Richardson <129875720+Rowen-Richardson@users.noreply.github.com>
2025-07-16 09:09:35 +0200

    Day 1 of 100 Python Challenge

---
+++ Day 1 - Print and Print Statments/main.py
@@ -0,0 +1,33 @@
+#Normal print to terminal statments 
+#These print statments line by line in py manually
+print("Hello Replit")
+print("how are you")
+print("I am fine 😘")
+
+#Print statments with new line or escape characters / we can print statments using one print function
+print("""Hello Replit 
+    Hope you\'re doing well today? 
+        I\'m having a dull day'""")
+
+"""
+The following print statments/function will not print on consle or terminal due to them not 
+being formated correctly 
+
+"""
+#print "Hello world"
+#print (Hello Python)
+#print ("Hello Row
+#print (Hello) + "Python"
+
+
+print("""Rowen
+July 16th 2025
+I\'m signing up for Raplit's 100 days of Python
+challenge!
+I will make sure to spend some time every day coding
+along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do 
+this from my phone wherever I happen to be. No excuses
+for not coding from the middle of a field or the wilderness!
+I am feeling 💪😍😃🥳
+Feel free to follow my progress at replit.com/@richardsonrowen """)
---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# 100 Days of Code - README
+
+Welcome my 100 Days of Coding - Python repository! This document outlines the progress and activities completed from Day 1 to Day 100.
+
+## Day 1 - Welcome to Python 
+
+- I've learned the python basics of print statments and functions 
+- Created a main.py file with correct syntax, how to print on terminal
+- The correct way to print multiple string texts and use Emoji's in python 🥳
