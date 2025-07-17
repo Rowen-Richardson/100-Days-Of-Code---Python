ffbff50dcf7c5bb2477b3b59cf3ea0804b114e52
Rowen Richardson <129875720+Rowen-Richardson@users.noreply.github.com>
2025-07-17 09:55:08 +0200

    Day 2 of 100 Python Challenge

---
+++ Day_2 - Input_and_Variables/input_and_variables.py
@@ -0,0 +1,86 @@
+#input("What's your name?: ")
+
+"""
+Variables are like name peacies or storage containors 
+where we can store or save things in but by no means is this the back-end
+or database back-end its just storge for current code that will be 
+forgoten once we stop running the code.
+
+how ever we can make calls to it currently when using the print statment
+
+"""
+#Basic variable storage without calls
+
+my_name = input("Name:")
+my_age = input("Age: ")
+print("Man your old")
+
+print()
+replit = input("How are you finding Replit so far? ")
+print("WE THOUGHT AS MUCH 🥳")
+
+
+#Making calls to our stored variables
+
+my_name = input("Name: ")
+my_age = input("Age: ")
+replit = input("How would you like Replit's help in your journy? ")
+replit_emotion = input("How do you feel about Replit? ")
+
+print()
+
+print("Your Name is ")
+print(my_name)
+print("And you want to be a Jedi at your age which is")
+print(my_age)
+
+print()
+
+print(replit)
+print("If that's the case then we can sure help you.")
+
+print()
+
+print("Cause clearly you feel")
+print(replit_emotion)
+print("About using Replit")
+
+
+#Getting to know you
+#This is a simple program to get to know you better
+
+print("Let's Get to know you!!!")
+
+persons_name = input("What 's your name? ")
+fav_food = input("What's your favourite food? ")
+music_listen_to = input("What's your favourite song and the Artist's name? ")
+persons_location = input("Where do you stay or Live? ")
+quote = """"You've gotta dance like there's nobody watching,
+Love like you'll never be hurt,
+Sing like there's nobody listening,
+And live like it's heaven on earth." 
+― William W. Purkey"""
+
+print()
+
+print("Nice to meet you ")
+print(persons_name)
+
+print()
+
+print("I'm guessing you'd like to have ")
+print(fav_food)
+
+print()
+
+print("That must be the greatest hit of all time we also like")
+print(music_listen_to)
+
+print()
+
+print("And we hope to pay you a vist in")
+print(persons_location)
+
+print()
+print(quote)
+
--- ReadMe.md
+++ ReadMe.md
@@ -7,3 +7,15 @@ Welcome my 100 Days of Coding - Python repository! This document outlines the pr
 - I've learned the python basics of print statments and functions 
 - Created a main.py file with correct syntax, how to print on terminal
 - The correct way to print multiple string texts and use Emoji's in python 🥳
+
+
+##  Day 2 - Input and Variablise
+
+- Learned more about the input function why we use it and its purpose.
+- Variables and how variables store states or information of what was input in the terminal.
+- Did a fun and quick getting to know you challenge.
+- Still printing everything out line by line and on Day 3 we shall clean it up
+
+<h4>The following highlighted text should take you to Replit so you can try out my code</h4>
+
+- 👨🏽‍💻 Getting to know you better [Click Me So I can get to know you better](https://replit.com/@richardsonrowen/day2100-days)
